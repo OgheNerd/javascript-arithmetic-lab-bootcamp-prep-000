f147f584dbc4081b1218ca93ee025ed35ccdabc1
Oghene Michael <zonalmeet@gmail.com>
2018-01-27 20:40:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ function add(x, y) {
 }
 
 function subtract(a, b) {
-  return a -b
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
 }
